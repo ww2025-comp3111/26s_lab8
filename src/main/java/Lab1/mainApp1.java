c487b395fd7ba7565e41958a48c72d9d105aeba6
ww2025-comp3111 <ustwwong+comp3111@gmail.com>
2026-03-31 09:29:15 +0800

    Conflict text B

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -7,8 +7,7 @@ package Lab1;
 public class mainApp1 {
     public static void main (String[] args){
         int b = 2;
-        int n = 11;
-        System.out.println("Conflict text A !");
+        int n = 12;
         System.out.println("Program ... Starting ...");
         System.out.println("1 + 1 = "+2);
         System.out.println(b+" to power " + n + " = "+myLibrary.Power(b,n));
